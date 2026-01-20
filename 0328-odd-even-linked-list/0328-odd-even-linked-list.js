cf20ba3c8d87159957934603941a826e90181f95
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-01-20 23:36:55 +0530

    Time: 0 ms (100%), Space: 58.5 MB (76.16%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.js
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.js
@@ -13,20 +13,19 @@ var oddEvenList = function (head) {
 
     if (!head || !head.next) return head;
 
-    let odd = head;          
-    let even = head.next;    
-    let evenHead = even;     
+    let odd = head;
+    let even = head.next;
+    let evenHead = even;
 
     while (even && even.next) {
-        odd.next = even.next;
-        odd = odd.next;
+        odd.next = odd.next.next;
+        even.next = even.next.next;
 
-        even.next = odd.next;
+        odd = odd.next;
         even = even.next;
     }
 
-    odd.next = evenHead; 
+    odd.next = evenHead;
     return head;
 
-    return sentinel.next
 };
