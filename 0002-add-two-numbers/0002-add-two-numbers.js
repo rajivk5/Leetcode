4e0e39968e7c63a02593130e6eb6d59b13211d25
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-01-21 21:10:36 +0530

    Time: 6 ms (12.58%), Space: 60.7 MB (90.84%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -9,28 +9,24 @@
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
- */var addTwoNumbers = function (l1, l2) {
-    const dummy = new ListNode(0);
-    let curr = dummy;
+ */
+var addTwoNumbers = function (l1, l2) {
+    let ans = new ListNode()
+    let ansHead = ans;
     let carry = 0;
+    while (l1 || l2 || carry) {
+        let sum = (!l1 ? 0 : l1.val) + (!l2 ? 0 : l2.val) + carry;
+        carry = Math.floor(sum / 10);
+        let digit = sum % 10;
 
-    while (l1 !== null || l2 !== null || carry !== 0) {
-        let sum = carry;
+        let newNode = new ListNode(digit)
+        ans.next = newNode;
+        ans = ans.next;
 
-        if (l1 !== null) {
-            sum += l1.val;
-            l1 = l1.next;
-        }
+        l1 = l1 && l1.next
+        l2 = l2 && l2.next
 
-        if (l2 !== null) {
-            sum += l2.val;
-            l2 = l2.next;
-        }
-
-        carry = sum >= 10 ? 1 : 0; 
-        curr.next = new ListNode(sum % 10);
-        curr = curr.next;
     }
 
-    return dummy.next;
+    return ansHead.next;
 };
