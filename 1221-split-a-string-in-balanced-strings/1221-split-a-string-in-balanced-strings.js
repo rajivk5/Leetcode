67a8a2ca4193b22aa39a52916ca9fff608e8371c
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-01-26 10:00:17 +0530

    Time: 0 ms (100%), Space: 54.1 MB (60.17%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
@@ -3,27 +3,12 @@
  * @return {number}
  */
 var balancedStringSplit = function (s) {
-    let L = 0;
-    let R = 0;
+    let balance = 0;
     let count = 0;
 
-    for (let i = 0;i<s.length;i++) {
-
-        if (s[i] == 'L') {
-            ++L
-
-        } else {
-            ++R
-
-        }
-
-        if (L == R) {
-            count++
-            L = 0;
-            R = 0;
-        }
-
-
+    for (let i = 0; i < s.length; i++) {
+        balance += (s[i] === 'L') ? 1 : -1;
+        if (balance === 0) count++;
     }
 
     return count;
