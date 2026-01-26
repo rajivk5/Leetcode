53c1193273806e4ccf4b6b9f9151773ec746ed00
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-01-27 01:20:16 +0530

    Time: 1 ms (79.5%), Space: 60.1 MB (20.31%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
@@ -3,9 +3,10 @@
  * @return {string}
  */
 var largestOddNumber = function (num) {
+
     for (let i = num.length - 1; i >= 0; i--) {
         if ((num[i] - '0') % 2 === 1) {
-            return num.slice(0, i + 1);
+            return num.substring(0, i + 1);
         }
     }
     return "";
