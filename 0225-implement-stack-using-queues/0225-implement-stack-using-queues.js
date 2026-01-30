eedd2ccca13c1199db0bc81d1553b31fcae63be8
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-01-30 22:57:53 +0530

    Time: 0 ms (100%), Space: 53.5 MB (76.34%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.js
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.js
@@ -1,7 +1,7 @@
 
 var MyStack = function () {
-    this.q1 = [];
-    this.q2 = [];
+    this.q = [];
+
 };
 
 /** 
@@ -9,7 +9,7 @@ var MyStack = function () {
  * @return {void}
  */
 MyStack.prototype.push = function (x) {
-    this.q1.push(x)
+    this.q.push(x)
 };
 
 /**
@@ -17,18 +17,16 @@ MyStack.prototype.push = function (x) {
  */
 MyStack.prototype.pop = function () {
 
-    let n = this.q1.length;
-    for (let i = 0; i < n - 1; i++) {
-        let frontEle = this.q1.shift();
-        this.q2.push(frontEle)
+    for (let i = 0; i < this.q.length - 1; i++) {
+
+        let front = this.q.shift()
+        this.q.push(front)
+
     }
-    let ans = this.q1.shift();
+    let ans = this.q[0]
+    this.q.shift()
+    return ans
 
-    let temp = this.q1;
-    this.q1 = this.q2;
-    this.q2 = temp;
-
-    return ans;
 };
 
 /**
@@ -36,19 +34,17 @@ MyStack.prototype.pop = function () {
  */
 MyStack.prototype.top = function () {
 
-    let n = this.q1.length;
-    for (let i = 0; i < n - 1; i++) {
-        let front = this.q1.shift();
-        this.q2.push(front)
+
+    for (let i = 0; i < this.q.length - 1; i++) {
+
+        let front = this.q.shift()
+        this.q.push(front)
+
     }
 
-    let ans = this.q1[0]
-    this.q2.push(this.q1.shift())
+    let ans = this.q[0]
 
-    let temp = this.q1;
-    this.q1 = this.q2;
-    this.q2 = temp;
-
+    this.q.push(this.q.shift())
 
     return ans
 
@@ -59,7 +55,7 @@ MyStack.prototype.top = function () {
  * @return {boolean}
  */
 MyStack.prototype.empty = function () {
-   return this.q1.length === 0
+    return this.q.length === 0
 };
 
 /** 
