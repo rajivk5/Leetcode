5e5cf5a1aa5312b0c05a4955727e98005972f484
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-02-01 17:54:13 +0530

    Time: 2 ms (86.03%), Space: 58.4 MB (23.85%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -6,40 +6,25 @@
 var nextGreaterElement = function (nums1, arr) {
     let map = {}
     let stack = [];
-    let n = arr.length;
 
-    stack.push(arr[n - 1]);
-    map[arr[n - 1]] = -1;
+    stack.push(arr[arr.length - 1]);
+    map[arr[arr.length - 1]] = -1;
 
 
-    for (let i = n - 2; i >= 0; i--) {
-        let top = stack[stack.length - 1]
+    for (let i = arr.length - 2; i >= 0; i--) {
 
-        if (arr[i] < top) {
-            map[arr[i]] = top;
-        } else {
-            while (stack.length) {
-                if (stack[stack.length - 1] < arr[i]) {
-                    stack.pop();
-                } else {
-                    map[arr[i]] = stack[stack.length - 1]
-                    break
-                }
-            }
-
-            if (stack.length === 0) {
-                map[arr[i]] = -1
+        while (stack.length) {
+          if (stack[stack.length - 1] < arr[i]) {
+                stack.pop();
+            } else {
+                map[arr[i]] = stack[stack.length - 1]
+                break
             }
         }
-
+        stack.length === 0?map[arr[i]] = -1:''
         stack.push(arr[i])
 
     }
 
-    let ans = []
-    for (let i = 0; i < nums1.length; i++) {
-        ans.push(map[nums1[i]])
-
-    }
-    return ans
+    return nums1.map(x=>map[x])
 };
