99778df4a987251acbf37c49b198513722bf2e10
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-02-03 01:42:29 +0530

    Time: 0 ms (100%), Space: 54.2 MB (19.7%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,15 +2,15 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
-      let n1 = 0;//1,1,2
-    let n2 = 1;//1,2,3
+var climbStairs = function (n) {
+    let pre = 1;
+    let curr = 1;
 
-    for (let i = 0; i <= n; i++) {
-        let n3 = n1 + n2;
-        n1 = n2;
-        n2 = n3
+    for (let i = 2; i <= n; i++) {
+        let temp = pre + curr;
+        pre = curr;
+        curr = temp;
     }
 
-    return n1;
+    return curr;
 };
