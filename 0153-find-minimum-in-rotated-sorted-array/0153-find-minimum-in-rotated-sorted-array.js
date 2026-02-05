56b93abe1a7e8b8764d3b98831d389b7d1d44abb
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-02-05 21:03:49 +0530

    Time: 0 ms (100%), Space: 54.1 MB (44.41%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -5,12 +5,13 @@
 var findMin = function (arr) {
     let l = 0;
     let r = arr.length - 1;
-    if (arr[l] <= arr[r]) return arr[l]
+    
 
     while (l <= r) {
+        if (arr[l] <= arr[r]) return arr[l]
+
         let m = Math.floor((l + r) / 2)
-        if(arr[m]<arr[m-1]) return arr[m]
-        if(arr[l]<arr[r]) return arr[l]
+        if (arr[m] < arr[m - 1]) return arr[m]
         if (arr[l] > arr[m]) {
             r = m - 1;
         } else {
