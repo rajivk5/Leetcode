76489135959095bf6d177ff03d275a4b7c43cf02
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-02-07 00:13:02 +0530

    Time: 0 ms (100%), Space: 54.5 MB (81.6%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -5,39 +5,43 @@
  */
 var searchRange = function (arr, t) {
 
-    
+
     function findFirst() {
         let l = 0;
         let r = arr.length - 1;
-        let ans = -1;
+        ans = -1;
         while (l <= r) {
-            let m = Math.floor((l + r) / 2)
-
-            if (arr[m] >= t) {
-                if (arr[m] === t) ans = m;
+            let m = Math.floor((l + r) / 2);
+            if (arr[m] > t) {
+                r = m - 1;
+            } else if (arr[m] < t) {
+                l = m + 1;
+            } else if (arr[m] === t) {
+                ans = m;
                 r = m - 1
-            } else {
-                l = m + 1
             }
         }
-        return ans;
+
+        return ans
     }
 
     function findSecond() {
         let l = 0;
         let r = arr.length - 1;
-        let ans = -1;
+        ans = -1;
         while (l <= r) {
-            let m = Math.floor((l + r) / 2)
-
-            if (arr[m] <= t) {
-                if (arr[m] === t) ans = m;
+            let m = Math.floor((l + r) / 2);
+            if (arr[m] > t) {
+                r = m - 1;
+            } else if (arr[m] < t) {
+                l = m + 1;
+            } else if (arr[m] === t) {
+                ans = m;
                 l = m + 1
-            } else {
-                r = m - 1
             }
         }
+
         return ans;
     }
-    return [findFirst(),findSecond()]
+    return [findFirst(), findSecond()]
 };
