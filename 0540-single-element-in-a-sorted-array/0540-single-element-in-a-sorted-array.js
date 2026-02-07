23534ae34b1a241d945ee6b8b1a8a143560cfea5
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-02-08 00:49:03 +0530

    Time: 0 ms (100%), Space: 57.3 MB (55.2%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -3,16 +3,28 @@
  * @return {number}
  */
 var singleNonDuplicate = function (arr) {
-    let i = 0;
-    let j = 1;
-    while (j) {
-        if (arr[i] === arr[j]) {
-            i += 2
-            j += 2
-        } else {
-            return arr[i];
+   let l = 0;
+    let r = arr.length - 1;
+
+    while (l < r) {
+        let m = Math.floor((l + r) / 2);
+        if(arr[m]!==arr[m-1] && arr[m]!==arr[m+1]) return arr[m]
+
+        if (arr[m] === arr[m - 1]) {
+            if ((m + l + 1) % 2 != 0) {
+                r = m - 2
+            } else {
+                l = m + 1
+            }
+        } else if (arr[m] === arr[m + 1]) {
+            if ((r + m + 1) % 2 !== 0) {
+                l = m + 2
+            }else{
+                r = m - 1;
+            }
         }
     }
+    return arr[l]
 };
 
 
