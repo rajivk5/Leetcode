e08d1880d5fb4d515651d4cf0dd3366bfbcc97b9
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-02-11 23:11:59 +0530

    Time: 18 ms (19.04%), Space: 60.5 MB (25.96%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.js
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -2,20 +2,19 @@
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(arr) {
+var trap = function (arr) {
+
     let maxL = [];
     maxL[0] = arr[0]
-    for (let i = 1; i < arr.length; i++) {
-        maxL[i] = Math.max(maxL[i - 1], arr[i])
-
-    }
-
     let maxR = [];
     maxR[arr.length - 1] = arr[arr.length - 1];
-    for (let j = arr.length - 2; j >= 0; j--) {
-        maxR[j] = Math.max(arr[j], maxR[j + 1])
+
+    for (let i = 1; i < arr.length; i++) {
+        maxL[i] = Math.max(maxL[i - 1], arr[i]);
+        maxR[arr.length - 1 - i] = Math.max(arr[arr.length - 1 - i], maxR[arr.length - i]);
 
     }
+
 
 
     let ans = 0;
