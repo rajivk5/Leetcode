5f931038f097f6af780e9ddfb7e92b777f679fae
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-02-15 00:18:04 +0530

    Time: 6 ms (97.39%), Space: 56.6 MB (64.44%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -4,33 +4,25 @@
  * @return {number}
  */
 var characterReplacement = function (s, k) {
-    let i = j = 0;
-    let map = Array(26).fill(0);
-    map[s.charCodeAt(0) - 65] = 1;
-    let maxWindow = 0;
-
-    while (j < s.length) {
-        if (isWindowValid(map, k)) {
-            maxWindow = Math.max(maxWindow, j - i + 1);
-            ++j;
-            ++map[s.charCodeAt(j) - 65];
-        } else {
-            --map[s.charCodeAt(i) - 65];
-            ++i
-        }
-    }
-
-    return maxWindow
-};
-
-var isWindowValid = function (map, k) {
-    let totalCount = 0;
+    let i = 0;
     let maxCount = 0;
+    let maxWindow = 0;
+    let map = Array(26).fill(0);
 
-    for (let i = 0; i < 26; i++) {
-        totalCount = totalCount + map[i];
-        maxCount = Math.max(maxCount, map[i])
+    for (let j = 0; j < s.length; j++) {
+        let index = s.charCodeAt(j) - 65;
+        map[index]++;
+
+        maxCount = Math.max(maxCount, map[index]);
+
+        // If window invalid → shrink
+        if ((j - i + 1) - maxCount > k) {
+            map[s.charCodeAt(i) - 65]--;
+            i++;
+        }
+
+        maxWindow = Math.max(maxWindow, j - i + 1);
     }
 
-    return (totalCount - maxCount <= k)
-}
+    return maxWindow;
+};
