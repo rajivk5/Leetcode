35aa8c6f41861f21b047f46a142728e6fc932e8c
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-02-22 21:42:52 +0530

    Time: 34 ms (72.85%), Space: 68.5 MB (47.65%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.js
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.js
@@ -5,21 +5,25 @@
 var totalFruit = function (fruits) {
     let i = 0;
     let maxLen = 0;
-    let map = {};
-    for (let j = 0; j < fruits.length; j++) {
-        map[fruits[j]] = (map[fruits[j]] || 0) + 1;
+    let map = new Map();
 
-        while (Object.keys(map).length > 2) {
-            map[fruits[i]]--;
-            if (map[fruits[i]] === 0) {
-                delete map[fruits[i]];
+    for (let j = 0; j < fruits.length; j++) {
+
+        map.set(fruits[j], (map.get(fruits[j]) || 0) + 1);
+
+        while (map.size > 2) {
+
+            map.set(fruits[i], map.get(fruits[i]) - 1);
+
+            if (map.get(fruits[i]) === 0) {
+                map.delete(fruits[i]);
             }
-            i++
+
+            i++;
         }
 
-        maxLen = Math.max(maxLen, j - i + 1)
-
+        maxLen = Math.max(maxLen, j - i + 1);
     }
 
-    return maxLen
+    return maxLen;
 };
