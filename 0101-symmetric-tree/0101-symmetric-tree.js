0d2a57ebd6a0e091e498dc2312002ed9b52e2400
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-03-03 00:05:42 +0530

    Time: 0 ms (100%), Space: 58.1 MB (12.44%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.js
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -10,7 +10,7 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isSymmetric = function (root) {
+/* var isSymmetric = function (root) {
     let isMirror = (left, right) => {
         if (!left && !right) return true;
         if (!left || !right) return false;
@@ -19,4 +19,20 @@ var isSymmetric = function (root) {
     }
 
     return isMirror(root.left, root.right)
-};
+}; */
+
+
+var isSymmetric = function (root) {
+    let q = [root.left, root.right];
+    while (q.length) {
+        let p1 = q.shift();
+        let p2 = q.shift();
+        if (!p1 && !p2) continue;
+        if (!p1 || !p2) return false;
+        if (p1.val !== p2.val) return false;
+        q.push(p1.left, p2.right);
+        q.push(p1.right, p2.left);
+    }
+
+    return true;
+}
