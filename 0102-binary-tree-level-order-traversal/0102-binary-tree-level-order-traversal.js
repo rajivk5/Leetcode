947e017acb12a0c03158b8b2eb92ba2723dc1f1d
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-03-02 13:33:36 +0530

    Time: 2 ms (46.44%), Space: 58.3 MB (70.13%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -10,7 +10,7 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrder = function (root) {
+/* var levelOrder = function (root) {
     if (!root) return [];
     let q = [root];
     let ans = [];
@@ -29,4 +29,21 @@ var levelOrder = function (root) {
     }
 
     return ans;
-};
+}; */
+
+
+var levelOrder = function (root) {
+    if (!root) return [];
+    let ans = [];
+
+    let traversal = (curr, level) => {
+        if (!ans[level]) ans[level] = [];
+
+        ans[level].push(curr.val);
+        curr.left && traversal(curr.left, level + 1);
+        curr.right && traversal(curr.right, level + 1);
+    }
+
+    traversal(root, 0);
+    return ans;
+}
