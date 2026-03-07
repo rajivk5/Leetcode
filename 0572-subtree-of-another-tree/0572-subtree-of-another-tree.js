5495fd22a02cbe52ebb147b68ee42e84b6be9739
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-03-07 23:54:09 +0530

    Time: 4 ms (86.94%), Space: 62.3 MB (65.41%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function (root, subRoot) {
+    let hashRoot = serialize(root);
+    let hashSubRoot = serialize(subRoot);
+
+   return hashRoot.includes(hashSubRoot);
+};
+
+let serialize = function (root) {
+    let hash = "";
+    let traverser = (curr) => {
+        if (!curr) {
+            hash += "-#";
+            return;
+        }
+        hash += '-' + curr.val;
+        traverser(curr.left)
+        traverser(curr.right)
+    }
+
+    traverser(root);
+    return hash;
+}
