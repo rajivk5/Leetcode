c82e02f56bf344775a8c2c8b54fae19bb818d612
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-03-08 23:23:36 +0530

    Time: 0 ms (100%), Space: 55.8 MB (5.77%) - LeetHub

---
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isUnivalTree = function (root) {
+    let value = root.val;
+
+    let traverse = (node) => {
+        if (!node) return true;
+
+        if (node.val !== value) return false;
+
+        return traverse(node.left) && traverse(node.right);
+    };
+
+    return traverse(root);
+};
