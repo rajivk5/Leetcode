f6977f0505adfd60d2b03da1569b1917d5487079
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-03-26 00:26:03 +0530

    Time: 9 ms (28.59%), Space: 56 MB (43.71%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.js
+++ 0043-multiply-strings/0043-multiply-strings.js
@@ -4,62 +4,33 @@
  * @return {string}
  */
 var multiply = function (num1, num2) {
-   if (num1 === "0" || num2 === "0") return "0";
+    if (num1 === "0" || num2 === "0") return "0";
 
-    let arr1 = num1.split('');
-    let arr2 = num2.split('');
-    let mulArr = [];
+    let n = num1.length;
+    let m = num2.length;
 
-    for (let i = arr2.length - 1; i >= 0; i--) {
-        let carry = 0;
-        let mulLevel = [];
+    let res = new Array(n + m).fill(0);
 
-        let zeros = arr2.length - 1 - i;
-        for (let z = 0; z < zeros; z++) {
-            mulLevel.push(0);
-        }
+    // right → left
+    for (let i = n - 1; i >= 0; i--) {
+        for (let j = m - 1; j >= 0; j--) {
 
-        for (let j = arr1.length - 1; j >= 0; j--) {
-            let mul = (arr2[i] - '0') * (arr1[j] - '0') + carry;
+            let mul = (num1[i] - '0') * (num2[j] - '0');
 
-            let digit = mul % 10;
-            carry = Math.floor(mul / 10);
+            let pos1 = i + j;
+            let pos2 = i + j + 1;
 
-            mulLevel.push(digit);
-        }
+            let sum = mul + res[pos2];
 
-        if (carry > 0) mulLevel.push(carry);
-        mulLevel.reverse();
-
-        mulArr.push(mulLevel);
-    }
-
-    return addtionArr(mulArr);
-};
-
-
-function addtionArr(arr) {
-    let maxLen = Math.max(...arr.map(row => row.length));
-    for (let row of arr) {
-        while (row.length < maxLen) {
-            row.unshift(0);
+            res[pos2] = sum % 10;                  // digit
+            res[pos1] += Math.floor(sum / 10);     // carry
         }
     }
 
-    let carry = 0;
-    let result = [];
-    for (let col = maxLen - 1; col >= 0; col--) {
-        let sum = carry;
-
-        for (let row = 0; row < arr.length; row++) {
-            sum += arr[row][col];
-        }
-
-        result.push(sum % 10);
-        carry = Math.floor(sum / 10);
+    // remove leading zeros
+    while (res[0] === 0) {
+        res.shift();
     }
 
-    if (carry > 0) result.push(carry);
-
-    return result.reverse().join('');
-};
+    return res.join('');
+}
