47fc8c202faff35ac8cb6cc1acba0cdee4d7bc30
Rajiv Kumar <107494710+rajivk5@users.noreply.github.com>
2026-03-27 00:03:53 +0530

    Time: 1 ms (99.25%), Space: 58.3 MB (75.54%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -3,56 +3,35 @@
  * @param {number[]} nums2
  * @return {number}
  */
+
+
 var findMedianSortedArrays = function (nums1, nums2) {
-    let arr = heapSort([...nums1, ...nums2])
+    let n = nums1.length;
+    let m = nums2.length;
 
-    let l = 0;
-    let r = arr.length - 1
+    let total = n + m;
+    let mid1 = Math.floor((total - 1) / 2);
+    let mid2 = Math.floor(total / 2);
 
-    if (arr.length % 2 == 0) {
-        let m1 = Math.floor((l + r) / 2);
-        let m2 = m1 + 1
-        let sum = arr[m1] + arr[m2]
-        return sum / 2
+    let i = 0, j = 0;
+    let count = 0;
+    let curr = 0, prev = 0;
+
+    while (count <= mid2) {
+        prev = curr;
+
+        if (i < n && (j >= m || nums1[i] <= nums2[j])) {
+            curr = nums1[i++];
+        } else {
+            curr = nums2[j++];
+        }
+
+        count++;
+    }
+
+    if (total % 2 === 0) {
+        return (prev + curr) / 2;
     } else {
-        let m = (l + r) / 2;
-        return arr[m];
+        return curr;
     }
 };
-
-
-
-
-function heapSort(arr) {
-    let n = arr.length;
-
-    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
-        heapifyDown(arr, i, n)
-    }
-
-    for (let i = n - 1; i > 0; i--) {
-        [arr[0], arr[i]] = [arr[i], arr[0]];
-        heapifyDown(arr, 0, i)
-    }
-
-    return arr;
-}
-
-function heapifyDown(arr, i, n) {
-    let largest = i;
-    let left = (2 * i) + 1;
-    let right = (2 * i) + 2;
-
-    if (left < n && arr[left] > arr[largest]) {
-        largest = left;
-    }
-    if (right < n && arr[right] > arr[largest]) {
-        largest = right;
-    }
-
-    if (largest != i) {
-        [arr[i], arr[largest]] = [arr[largest], arr[i]];
-        heapifyDown(arr, largest, n)
-    }
-
-}
